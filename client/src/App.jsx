87e44aadae5d8e9b3cbc698c6a65ba594e72b383
KenleyLuk <kentyhung@gmail.com>
2024-11-18 21:09:20 -0500

    format: format the code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,30 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-
-import uploadImage from "./assets/upload/image.png"; // Import the image file
-
-import "./UploadImage.css"; // Import CSS file for styling
+import "./UploadImage.css";
+import uploadImage from "./assets/upload/image.png";
 
 function App() {
+  // State variables
   const [selectedFile, setSelectedFile] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
 
+  // Fetch API data on component mount
   useEffect(() => {
-    const fetchAPI = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get(
           "http://localhost:8080/api/get-all-images"
         );
-        console.log(response.data);
+        console.log("API response:", response.data);
       } catch (error) {
-        console.error(error);
+        console.error("Error fetching API data:", error);
       }
     };
 
-    fetchAPI();
-  }, []); // 空数组表示只在组件挂载时调用一次
+    fetchData();
+  }, []);
 
+  // Handle file input change
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
@@ -39,22 +38,25 @@ function App() {
     }
   };
 
+  // Prevent default behavior on drag over
   const handleDragOver = (event) => {
-    console.log("handleDragOver")
     event.preventDefault();
   };
 
+  // Handle file drop
   const handleDrop = (event) => {
     event.preventDefault();
     const file = event.dataTransfer.files[0];
-    const fakeEvent = { target: { files: [file] } }; // Creating a fake event object with the file
+    const fakeEvent = { target: { files: [file] } };
     handleFileChange(fakeEvent);
-};
+  };
 
+  // Reset selected file
   const handleReset = () => {
     setSelectedFile(null);
   };
 
+  // Upload selected file
   const handleUpload = async () => {
     if (selectedFile) {
       const formData = new FormData();
@@ -79,6 +81,7 @@ function App() {
     }
   };
 
+  // Trigger file input click
   const handleClickUploadText = () => {
     document.getElementById("fileInput").click();
   };
@@ -86,22 +89,24 @@ function App() {
   return (
     <section className="body">
       <header>
-        <div className="title">S3 file upload directive</div>
-        <div className="subtitle">
-          A simple directive to create a customizable file input control
-        </div>
+        <div className="title">S3 File Upload Directive</div>
+        <div className="subtitle">Create a customizable file input control</div>
       </header>
 
-      <div className="upload-section" onDragOver={handleDragOver} onDrop={handleDrop}>
+      <div
+        className="upload-section"
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
         <div className="button-container">
-          <img className="uploadImage" src={uploadImage} alt="image icon" />
+          <img className="uploadImage" src={uploadImage} alt="Image Icon" />
           <div className="dragTextContainer">
             <div className="uploadText" onClick={handleClickUploadText}>
-              Click to upload
+              Click to Upload
             </div>
-            <div className="dropText">or drag and drop</div>
+            <div className="dropText">or Drag and Drop</div>
           </div>
-          <div className="maximumFileTtext">Maximum file size 5MB</div>
+          <div className="maximumFileSizeText">Maximum file size 5MB</div>
         </div>
         <input
           type="file"
@@ -110,12 +115,16 @@ function App() {
           onChange={handleFileChange}
         />
       </div>
-    
-        {selectedFile && (
-          <div>
-            <img className="uploadedContainer" src={imagePreview} alt="Selected file" />
-          </div>
-        )}
+
+      {selectedFile && (
+        <div>
+          <img
+            className="uploadedContainer"
+            src={imagePreview}
+            alt="Selected File Preview"
+          />
+        </div>
+      )}
 
       <div className="submit-container">
         <button className="submit-button" onClick={handleUpload}>
